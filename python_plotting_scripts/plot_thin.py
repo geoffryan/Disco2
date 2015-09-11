dfc87b4ad015de0dd8c66b75512af3e12ee01ff5
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2015-09-11 01:27:00 -0400

    plot_thin has NT and plunge solution for Kerr

--- python_plotting_scripts/plot_thin.py
+++ python_plotting_scripts/plot_thin.py
@@ -16,6 +16,67 @@ rho_scale = 1.0
 
 nu = np.logspace(14,20,num=7,base=10.0)
 
+blue = (31.0/255, 119.0/255, 180.0/255)
+orange = (255.0/255, 127.0/255, 14.0/255)
+green = (44.0/255, 160.0/255, 44.0/255)
+red = (214.0/255, 39.0/255, 40.0/255)
+purple = (148.0/255, 103.0/255, 189.0/255)
+
+def P_gas(g, rho, T):
+    return rho * T
+
+def P_rad(g, rho, T):
+    return 4.0*sb/(3.0*c) * (T*mp*c*c)**4 / (c*c)
+
+def P_deg(g, rho, T):
+    return 2*np.pi*h*c/3.0 * np.power(3*rho/(8*np.pi*mp),4.0/3.0) / (c*c)
+
+def e_gas(g, rho, T):
+    GAM = g._pars['Adiabatic_Index']
+    return T / (GAM-1.0)
+
+def e_rad(g, rho, T):
+    return 4.0*sb * (T*mp*c*c)**4 / (c*rho*c*c)
+
+def e_deg(g, rho, T):
+    return h*c/(4.0*mp) * np.power(3*rho/(8*np.pi*mp),1.0/3.0) / (c*c)
+
+def ppp(g, rho, T):
+    EOSType = g._pars['EOSType']
+    EOSPar1 = g._pars['EOSPar1']
+    EOSPar2 = g._pars['EOSPar2']
+    EOSPar3 = g._pars['EOSPar3']
+
+    P = np.zeros(rho.shape)
+
+    if EOSType == 0:
+        P = P_gas(g, rho, T)
+    elif EOSType == 1:
+        P = EOSPar1 * P_gas(g, rho, T) + EOSPar2 * P_rad(g, rho, T)
+    else:
+        P = EOSPar1*P_gas(g,rho,T) + EOSPar2*P_rad(g,rho,T)\
+            + EOSPar3*P_deg(g,rho,T)
+
+    return P
+
+def eps(g, rho, T):
+    EOSType = g._pars['EOSType']
+    EOSPar1 = g._pars['EOSPar1']
+    EOSPar2 = g._pars['EOSPar2']
+    EOSPar3 = g._pars['EOSPar3']
+
+    e = np.zeros(rho.shape)
+
+    if EOSType == 0:
+        e = e_gas(g, rho, T)
+    elif EOSType == 1:
+        e = EOSPar1 * e_gas(g, rho, T) + EOSPar2 * e_rad(g, rho, T)
+    else:
+        e = EOSPar1*e_gas(g,rho,T) + EOSPar2*e_rad(g,rho,T)\
+            + EOSPar3*e_deg(g,rho,T)
+
+    return e
+
 def gToArr(g):
     R = np.zeros(g.np.sum())
     prim = np.zeros((g.np.sum(),5))
@@ -48,12 +109,66 @@ def ISCO(M, a):
 
     return Risco
 
+def allTheThings(g, r, prim):
+
+    M = g._pars['GravM']
+    a = g._pars['GravA']
+    BG = g._pars['Background']
+
+    if BG == 3:
+        rho = prim[:,0]
+        T = prim[:,1]
+        vr = prim[:,2]
+        vp = prim[:,3]
+
+        u0 = 1.0 / np.sqrt(1 - 2*M/r - 4*M/r*vr + 4*M*a/r*vp 
+                - (1+2*M/r)*vr*vr + 2*(1+2*M/r)*a*vr*vp
+                - (r*r+a*a+2*M*a*a/r)*vp*vp)
+        p = ppp(g, rho, T)
+        e = eps(g, rho, T)
+        rhoh = rho + rho*e + p
+
+        H = np.sqrt(r*r*r*p/(M*rhoh)) / u0
+        sig = rho*H
+        pi = p*H
+        mdot = -2*np.pi*r*sig*u0*vr
+        
+        qdot = 8*sb * (T*mp*c*c)**4 / (3*ka_bbes*sig * c*c*c*rho_scale*r_scale)
+
+
+    else:
+        sig = prim[:,0]
+        pi = prim[:,1]
+        vr = prim[:,2]
+        vp = prim[:,3]
+        
+        u0 = 1.0 / np.sqrt(1 - 2*M/r - 4*M/r*vr + 4*M*M*a/r*vp 
+                - (1+2*M/r)*vr*vr + 2*(1+2*M/r)*M*a*vr*vp
+                - (r*r+a*a*M*M+2*M*M*M*a*a/r)*vp*vp)
+        mdot = -2*np.pi*r*sig*u0*vr
+
+        H = np.sqrt(r*r*r*pi/(M*sig)) / u0
+        rho = sig/H
+        p = pi/H
+
+        T = mp*c*c * pi/sig
+        qdot = 8*sb * T*T*T*T / (3*ka_bbes*sig * c*c*c*rho_scale*r_scale)
+
+    return rho, sig, p, pi, H, T, vr, vp, mdot, qdot
+
 def calcNT(g, r, rs, Mdot):
 
     M = g._pars['GravM']
     a = g._pars['GravA']
     GAM = g._pars['Adiabatic_Index']
     AL = g._pars['AlphaVisc']
+
+    EOSType = g._pars['EOSType']
+    EOSPar2 = g._pars['EOSPar2']
+
+    rad = False
+    if EOSType == 1 and EOSPar2 == 1.0:
+        rad = True
 
     A = M*a
     OMK = np.sqrt(M/(r*r*r))
@@ -103,12 +218,20 @@ def calcNT(g, r, rs, Mdot):
                     / (c*c*c*r_scale*rho_scale), 0.2)
     SIG = np.power(8.0*sb/(3*ka_bbes*QDOT) * (PI*mp*c*c)**4
                     / (c*c*c*r_scale*rho_scale), 0.2)
-
     vr = -Mdot / (2*np.pi*r*sig)
     VR = -Mdot / (2*np.pi*r*SIG * U0)
 
     vp = OMK
     VP = OMK / B
+
+    if rad:
+#        SIGrad = 8.0 * (PI*rho_scale*c*c*r_scale)\
+#                / (ka_bbes * QDOT*rho_scale*c*c*c) / (rho_scale*r_scale)
+        SIGrad = 8.0 * (PI) / (ka_bbes * rho_scale*r_scale * QDOT)
+        VRrad = -Mdot / (2*np.pi*r*SIGrad * U0)
+        
+        return (sig, pi, vr, vp, Qdot), (SIG, PI, VR, VP, QDOT), \
+                (SIGrad, PI, VRrad, VP, QDOT)
 
     return (sig, pi, vr, vp, Qdot), (SIG, PI, VR, VP, QDOT)
 
@@ -134,9 +257,24 @@ def calcGEO(g, r, Mdot, K):
         EPS = -(1-2*M/Risco)*U0 - 2*M*M*A/Risco*UP
         LLL = -2*M*M*A/Risco*U0 + (Risco*Risco+M*M*A*A+2*M*M*M*A*A/Risco)*UP
 
-        ur = np.zeros(r.shape)
-        up = np.zeros(r.shape)
-        u0 = np.zeros(r.shape)
+        #Inverse Metric components
+        g00 = -1-2*M/r
+        g0r = 2*M/r
+        g0p = np.zeros(r.shape)
+        grr = 1 - 2*M/r + A*A*M*M/(r*r)
+        grp = A*M/(r*r)
+        gpp = 1.0/(r*r)
+
+        u0d = EPS*np.ones(r.shape)
+        upd = LLL*np.ones(r.shape)
+        a = grr
+        b = g0r*u0d + grp*upd
+        c = g00*u0d*u0d + 2*g0p*u0d*upd + gpp*upd*upd + 1.0
+        urd = (-b - np.sqrt(b*b - a*c)) / a
+
+        u0 = g00*u0d + g0r*urd + g0p*upd
+        ur = g0r*u0d + grr*urd + grp*upd
+        up = g0p*u0d + grp*urd + gpp*upd
 
 
     vr = ur/u0
@@ -148,109 +286,170 @@ def calcGEO(g, r, Mdot, K):
 
     return rho, P, vr, vp
 
-def plotNT(g):
+def plotSigNice(g):
 
     r, prim = gToArr(g)
 
+    rho, sig, p, pi, H, T, vr, vp, mdot, qdot = allTheThings(g, r, prim)
+
     M = g._pars['GravM']
     a = g._pars['GravA']
+    EOSType = g._pars['EOSType']
+    EOSPar2 = g._pars['EOSPar2']
+    rad = False
+    if EOSType == 1 and EOSPar2 == 1.0:
+        rad = True
+
     Risco = ISCO(M,a)
 
     R = np.logspace(math.log10(Risco)+0.01, math.log10(r.max()), base=10.0, num=200)
     R2 = np.logspace(math.log10(r.min()), math.log10(Risco)-0.05, base=10.0, num=200)
 
     RS = Risco
-    MDOT = 174
-    K = 2.5e-5
+    MDOT = 270
+    K = 3.5e-5
 
-    SSdat, NTdat = calcNT(g, R, RS, MDOT)
+    if rad:
+        SSdat, NTdat, NTraddat = calcNT(g, R, RS, MDOT)
+    else:
+        SSdat, NTdat = calcNT(g, R, RS, MDOT)
+
     GEOdat = calcGEO(g, R2, MDOT, K)
 
     if a != 0.0:
         g._pars['GravA'] = 0.0
         R3 = np.logspace(math.log10(6*M)+0.01, math.log10(r.max()), base=10.0, num=200)
         SSdat2, NTdat2 = calcNT(g, R3, 6*M, MDOT)
+        g._pars['GravA'] = a
+
+    fig, ax = plt.subplots(1,1, figsize=(14,9))
+
+    xlim = (1.0, 1.0e2)
+
+    ax.plot(r, sig * rho_scale*r_scale, 'k+', label='Disco')
+    #ax[0,0].plot(R, SSdat[0], ls='-', lw=3.0, color=orange)
+    ax.plot(R2, GEOdat[0], ls='-', lw=3.0, color=green, label='Plunge')
+    ax.plot(R, NTdat[0], ls='-', lw=3.0, color=blue, label='Novikov-Thorne')
+    #if a != 0.0:
+    #    ax.plot(R3, NTdat2[0], ls='-', lw=3.0, color=red)
+    #if rad:
+    #    ax.plot(R, NTraddat[0], ls='-', lw=3.0, color=purple)
+    ax.set_xlabel(r"$r$ $(GM_\odot / c^2)$")
+    ax.set_ylabel(r"$\Sigma$ $(g/cm^2)$")
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlim(xlim)
+
+    return fig, ax
+
+def plotNT(g):
+
+    r, prim = gToArr(g)
+
+    rho, sig, p, pi, H, T, vr, vp, mdot, qdot = allTheThings(g, r, prim)
+
+    M = g._pars['GravM']
+    a = g._pars['GravA']
+    EOSType = g._pars['EOSType']
+    EOSPar2 = g._pars['EOSPar2']
+    rad = False
+    if EOSType == 1 and EOSPar2 == 1.0:
+        rad = True
+
+    Risco = ISCO(M,a)
+
+    R = np.logspace(math.log10(Risco)+0.01, math.log10(r.max()), base=10.0, num=200)
+    R2 = np.logspace(math.log10(r.min()), math.log10(Risco)-0.05, base=10.0, num=200)
+
+    RS = Risco
+    MDOT = 270
+    K = 3.5e-5
+
+    if rad:
+        SSdat, NTdat, NTraddat = calcNT(g, R, RS, MDOT)
+    else:
+        SSdat, NTdat = calcNT(g, R, RS, MDOT)
+
+    GEOdat = calcGEO(g, R2, MDOT, K)
+
+    if a != 0.0:
+        g._pars['GravA'] = 0.0
+        R3 = np.logspace(math.log10(6*M)+0.01, math.log10(r.max()), base=10.0, num=200)
+        SSdat2, NTdat2 = calcNT(g, R3, 6*M, MDOT)
+        g._pars['GravA'] = a
 
     fig, ax = plt.subplots(2,3, figsize=(14,9))
 
-    print r.shape
-    print prim.shape
-    print R.shape
-    print SSdat[0].shape
-
-    blue = (31.0/255, 119.0/255, 180.0/255)
-    orange = (255.0/255, 127.0/255, 14.0/255)
-    green = (44.0/255, 160.0/255, 44.0/255)
-    red = (214.0/255, 39.0/255, 40.0/255)
-
     xlim = (1.0, 1.0e3)
 
-    ax[0,0].plot(r, prim[:,0], 'k+')
+    ax[0,0].plot(r, sig, 'k+')
     ax[0,0].plot(R, SSdat[0], ls='-', lw=3.0, color=orange)
     ax[0,0].plot(R2, GEOdat[0], ls='-', lw=3.0, color=green)
     ax[0,0].plot(R, NTdat[0], ls='-', lw=3.0, color=blue)
     if a != 0.0:
         ax[0,0].plot(R3, NTdat2[0], ls='-', lw=3.0, color=red)
+    if rad:
+        ax[0,0].plot(R, NTraddat[0], ls='-', lw=3.0, color=purple)
     ax[0,0].set_xlabel(r"$r$")
     ax[0,0].set_ylabel(r"$\Sigma$")
     ax[0,0].set_xscale('log')
     ax[0,0].set_yscale('log')
     ax[0,0].set_xlim(xlim)
     
-    ax[0,1].plot(r, prim[:,1], 'k+')
+    ax[0,1].plot(r, pi, 'k+')
     ax[0,1].plot(R, SSdat[1], ls='-', lw=3.0, color=orange)
     ax[0,1].plot(R2, GEOdat[1], ls='-', lw=3.0, color=green)
     ax[0,1].plot(R, NTdat[1], ls='-', lw=3.0, color=blue)
     if a != 0.0:
         ax[0,1].plot(R3, NTdat2[1], ls='-', lw=3.0, color=red)
+    if rad:
+        ax[0,1].plot(R, NTraddat[1], ls='-', lw=3.0, color=purple)
     ax[0,1].set_xlabel(r"$r$")
     ax[0,1].set_ylabel(r"$\Pi$")
     ax[0,1].set_xscale('log')
     ax[0,1].set_yscale('log')
     ax[0,1].set_xlim(xlim)
 
-    ax[1,0].plot(r, -prim[:,2], 'k+')
+    ax[1,0].plot(r, -vr, 'k+')
     ax[1,0].plot(R, -SSdat[2], ls='-', lw=3.0, color=orange)
     ax[1,0].plot(R2, -GEOdat[2], ls='-', lw=3.0, color=green)
     ax[1,0].plot(R, -NTdat[2], ls='-', lw=3.0, color=blue)
     if a != 0.0:
         ax[1,0].plot(R3, -NTdat2[2], ls='-', lw=3.0, color=red)
+    if rad:
+        ax[1,0].plot(R, -NTraddat[2], ls='-', lw=3.0, color=purple)
     ax[1,0].set_xlabel(r"$r$")
     ax[1,0].set_ylabel(r"$v^r$")
     ax[1,0].set_xscale('log')
     ax[1,0].set_yscale('log')
     ax[1,0].set_xlim(xlim)
 
-    ax[1,1].plot(r, prim[:,3], 'k+')
+    ax[1,1].plot(r, vp, 'k+')
     ax[1,1].plot(R, SSdat[3], ls='-', lw=3.0, color=orange)
     ax[1,1].plot(R2, GEOdat[3], ls='-', lw=3.0, color=green)
     ax[1,1].plot(R, NTdat[3], ls='-', lw=3.0, color=blue)
     if a != 0.0:
         ax[1,1].plot(R3, NTdat2[3], ls='-', lw=3.0, color=red)
+    if rad:
+        ax[1,1].plot(R, NTraddat[3], ls='-', lw=3.0, color=purple)
     ax[1,1].set_xlabel(r"$r$")
     ax[1,1].set_ylabel(r"$v^\phi$")
     ax[1,1].set_xscale('log')
     ax[1,1].set_yscale('log')
     ax[1,1].set_xlim(xlim)
     
-    sig = prim[:,0]
-    pi = prim[:,1]
-    vr = prim[:,2]
-    vp = prim[:,3]
-    T = mp*c*c * pi/sig
-    qdot = 8*sb * T*T*T*T / (3*ka_bbes*sig * c*c*c*rho_scale*r_scale)
-
     ax[0,2].plot(r, qdot, 'k+')
     ax[0,2].plot(R, SSdat[4], ls='-', lw=3.0, color=orange)
     ax[0,2].plot(R, NTdat[4], ls='-', lw=3.0, color=blue)
+    if a != 0.0:
+        ax[0,2].plot(R3, NTdat2[4], ls='-', lw=3.0, color=red)
+    if rad:
+        ax[0,2].plot(R, NTraddat[4], ls='-', lw=3.0, color=purple)
     ax[0,2].set_xlabel(r"$r$")
     ax[0,2].set_ylabel(r"$\dot{Q}$")
     ax[0,2].set_xscale('log')
     ax[0,2].set_yscale('log')
     ax[0,2].set_xlim(xlim)
-
-    u0 = 1.0 / np.sqrt(1 - 2*M/r - 4*M/r*vr - (1+2*M/r)*vr*vr - r*r*vp*vp)
-    mdot = -2*np.pi*r*sig*u0*vr
 
     ax[1,2].plot(r, mdot, 'k+')
     ax[1,2].plot(R, MDOT*np.ones(R.shape), ls='-', lw=3.0, color=orange)
@@ -263,19 +462,47 @@ def plotNT(g):
 
     plt.tight_layout()
 
-    fig.savefig("thin.png")
+    return fig, ax
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("Need a parfile and checkpoint, numbnuts!")
 
+    print("Making grid.")
     par = dp.readParfile(sys.argv[1])
     g = dp.Grid(par)
-    g.loadCheckpoint(sys.argv[2])
 
-    plotNT(g)
+    if len(sys.argv) == 3:
 
-    plt.show()
+        checkpoint = sys.argv[2]
+        print("Loading {0:s}".format(checkpoint))
+        g.loadCheckpoint(sys.argv[2])
+        num = int(sys.argv[2].split(".")[-2].split("_")[-1])
+        name = "plot_thin_{0:04d}.png".format(num)
+
+        print("   Plotting {0:s}".format(checkpoint))
+
+        fig, ax = plotNT(g)
+        print("   Saving {0:s}".format(name))
+        fig.savefig(name)
+
+        plt.show()
+
+    else:
+
+        for checkpoint in sys.argv[2:]:
+
+            print("Loading {0:s}".format(checkpoint))
+            g.loadCheckpoint(checkpoint)
+            num = int(checkpoint.split(".")[-2].split("_")[-1])
+            name = "plot_thin_{0:04d}.png".format(num)
+            
+            print("   Plotting {0:s}".format(checkpoint))
+            fig, ax = plotNT(g)
+            
+            print("   Saving {0:s}".format(name))
+            fig.savefig(name)
+            plt.close()
 
 
