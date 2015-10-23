372e6e89dbfc0aca346e3a9b3d04b43549cd147d
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2015-10-23 14:24:45 -0400

    Updated DiagEquat plotting.

--- python_plotting_scripts/plot_DiagEquat.py
+++ python_plotting_scripts/plot_DiagEquat.py
@@ -3,13 +3,42 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
+import matplotlib.colors as clrs
+import matplotlib.ticker as tkr
 import discopy as dp
 
-RMAX = 10.0
+RMAX = 3.5
+scale = "log"
+
+def makePlot(fig, ax, mesh, dat, scale="linear", title="", label="", **kwargs):
+
+    N = 200
+
+    if scale == "log":
+        v = np.logspace(math.floor(math.log10(dat.min())),
+                        math.ceil(math.log10(dat.max())), base=10.0, num=N)
+        norm = clrs.LogNorm()
+        locator = tkr.LogLocator()
+        formatter = tkr.LogFormatter()
+    else:
+        v = np.linspace(dat.min(), dat.max(), num=N)
+        norm = clrs.Normalize()
+        locator = tkr.AutoLocator()
+        formatter = tkr.ScalarFormatter()
+
+    C = ax.tricontourf(mesh, dat, v, norm=norm, **kwargs)
+    colorbar = fig.colorbar(C, format=formatter, ticks=locator)
+
+    ax.set_xlabel(r"$x$")
+    ax.set_ylabel(r"$y$")
+    ax.set_aspect('equal')
+    colorbar.set_label(label)
+    fig.suptitle(title, fontsize=24)
 
 def plotDiagEquat(filename):
-
+    
     t, r, phi, rho, P, vr, vp, vz, w, dV = dp.readDiagEquat(filename)
+
 
     inds = r<RMAX
 
@@ -17,17 +46,62 @@ def plotDiagEquat(filename):
     y = r[inds] * np.sin(phi[inds])
     mesh = tri.Triangulation(x, y)
 
+    title = r"$t = {0:.2e}$".format(t)
+
     fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, rho[inds], scale="linear", title=title,    
+                label=r"$\Sigma$", cmap=plt.cm.afmhot)
+    fig.savefig("rho_lab_{0:010.2f}.png".format(t))
+    plt.close(fig)
 
-    C = ax.tricontourf(mesh, rho[inds], 200, cmap=plt.cm.afmhot)
-    colorbar = fig.colorbar(C)
+    fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, rho[inds], scale="log", title=title,    
+                label=r"$\Sigma$", cmap=plt.cm.afmhot)
+    fig.savefig("log_rho_lab_{0:010.2f}.png".format(t))
+    plt.close(fig)
 
-    ax.set_xlabel(r"$x$")
-    ax.set_ylabel(r"$y$")
-    fig.suptitle(r"$t = {0:.2g}$".format(t), fontsize=24)
+    fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, P[inds]/rho[inds], scale="linear", title=title,    
+                label=r"$P / \rho$", cmap=plt.cm.afmhot)
+    fig.savefig("P_o_rho_lab_{0:010.2f}.png".format(t))
+    plt.close(fig)
 
-    fig.savefig("rho_{0:010.2f}.png".format(t))
-    plt.close()
+    fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, P[inds]/rho[inds], scale="log", title=title,    
+                label=r"$P / \rho$", cmap=plt.cm.afmhot)
+    fig.savefig("log_P_o_rho_lab_{0:010.2f}.png".format(t))
+    plt.close(fig)
+
+
+    phi -= t
+    x = r[inds] * np.cos(phi[inds])
+    y = r[inds] * np.sin(phi[inds])
+    mesh = tri.Triangulation(x, y)
+
+    fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, rho[inds], scale="linear", title=title,    
+                label=r"$\Sigma$", cmap=plt.cm.afmhot)
+    fig.savefig("rho_com_{0:010.2f}.png".format(t))
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, rho[inds], scale="log", title=title,    
+                label=r"$\Sigma$", cmap=plt.cm.afmhot)
+    fig.savefig("log_rho_com_{0:010.2f}.png".format(t))
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, P[inds]/rho[inds], scale="linear", title=title,    
+                label=r"$P / \rho$", cmap=plt.cm.afmhot)
+    fig.savefig("P_o_rho_com_{0:010.2f}.png".format(t))
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    makePlot(fig, ax, mesh, P[inds]/rho[inds], scale="log", title=title,    
+                label=r"$P / \rho$", cmap=plt.cm.afmhot)
+    fig.savefig("log_P_o_rho_com_{0:010.2f}.png".format(t))
+    plt.close(fig)
+
 
 if __name__ == "__main__":
 
